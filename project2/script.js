3dd2ba3366773baceff88fa483e6f51678aec454
Helen Wu <wuz420@newschool.edu>
2024-02-27 20:25:27 -0500

    Added songs into playlist

--- project2/script.js
+++ project2/script.js
@@ -5,44 +5,61 @@
 
 $("document").ready(function(){
 
+  $(".hours").draggable();
+
   // var soundClick = document.getElementById("music1");
+
+  // $("#play1").click(function(){
+  //   $("audio#music1")[0].play();
+  //   $("#play1").css({
+  //     "visibility":"hidden",
+  //   });
+  //   $("#pause1").css({
+  //     "visibility":"visible",
+  //   });
+  //   $("#st1").css({
+  //     "visibility":"visible",
+  //   });
+  // });
 
   $("#play1").click(function(){
     $("audio#music1")[0].play();
-    $("#play1").css({
-      "visibility":"hidden",
-    });
-    $("#pause1").css({
-      "visibility":"visible",
-    });
+    $("#play1").toggle();
+    $("#pause1").toggle();
     $("#st1").css({
       "visibility":"visible",
     });
   });
-
 
   $("#pause1").click(function(){
     $("audio#music1")[0].pause();
-    $("#play1").css({
-      "visibility":"visible",
-    });
-    $("#pause1").css({
-      "visibility":"hidden",
-    })
+    $("#play1").toggle();
+    $("#pause1").toggle();
     $("#st1").css({
       "visibility":"hidden",
     });
-
   });
+
+
+  // $("#pause1").click(function(){
+  //   $("audio#music1")[0].pause();
+  //   $("#play1").css({
+  //     "visibility":"visible",
+  //   });
+  //   $("#pause1").css({
+  //     "visibility":"hidden",
+  //   })
+  //   $("#st1").css({
+  //     "visibility":"hidden",
+  //   });
+
+  // });
+
 
   $("#play2").click(function(){
     $("audio#music2")[0].play();
-    $("#play2").css({
-      "visibility":"hidden",
-    });
-    $("#pause2").css({
-      "visibility":"visible",
-    });
+    $("#play2").toggle();
+    $("#pause2").toggle();
     $("#st2").css({
       "visibility":"visible",
     });
@@ -50,18 +67,318 @@ $("document").ready(function(){
 
   $("#pause2").click(function(){
     $("audio#music2")[0].pause();
-    $("#play2").css({
-      "visibility":"visible",
-    });
-    $("#pause2").css({
-      "visibility":"hidden",
-    })
+    $("#play2").toggle();
+    $("#pause2").toggle();
     $("#st2").css({
       "visibility":"hidden",
     });
 
   });
 
+  $("#play3").click(function(){
+    $("audio#music3")[0].play();
+    $("#play3").toggle();
+    $("#pause3").toggle();
+    $("#st3").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause3").click(function(){
+    $("audio#music3")[0].pause();
+    $("#play3").toggle();
+    $("#pause3").toggle();
+    $("#st3").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play4").click(function(){
+    $("audio#music4")[0].play();
+    $("#play4").toggle();
+    $("#pause4").toggle();
+    $("#st4").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause4").click(function(){
+    $("audio#music4")[0].pause();
+    $("#play4").toggle();
+    $("#pause4").toggle();
+    $("#st4").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play5").click(function(){
+    $("audio#music5")[0].play();
+    $("#play5").toggle();
+    $("#pause5").toggle();
+    $("#st5").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause5").click(function(){
+    $("audio#music5")[0].pause();
+    $("#play5").toggle();
+    $("#pause5").toggle();
+    $("#st5").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play6").click(function(){
+    $("audio#music6")[0].play();
+    $("#play6").toggle();
+    $("#pause6").toggle();
+    $("#st6").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause6").click(function(){
+    $("audio#music6")[0].pause();
+    $("#play6").toggle();
+    $("#pause6").toggle();
+    $("#st6").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play7").click(function(){
+    $("audio#music7")[0].play();
+    $("#play7").toggle();
+    $("#pause7").toggle();
+    $("#st7").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause7").click(function(){
+    $("audio#music7")[0].pause();
+    $("#play7").toggle();
+    $("#pause7").toggle();
+    $("#st7").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+
+  $("#play8").click(function(){
+    $("audio#music8")[0].play();
+    $("#play8").toggle();
+    $("#pause8").toggle();
+    $("#st8").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause8").click(function(){
+    $("audio#music8")[0].pause();
+    $("#play8").toggle();
+    $("#pause8").toggle();
+    $("#st8").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play9").click(function(){
+    $("audio#music9")[0].play();
+    $("#play9").toggle();
+    $("#pause9").toggle();
+    $("#st9").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause9").click(function(){
+    $("audio#music9")[0].pause();
+    $("#play9").toggle();
+    $("#pause9").toggle();
+    $("#st9").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play10").click(function(){
+    $("audio#music10")[0].play();
+    $("#play10").toggle();
+    $("#pause10").toggle();
+    $("#st10").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause10").click(function(){
+    $("audio#music10")[0].pause();
+    $("#play10").toggle();
+    $("#pause10").toggle();
+    $("#st10").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play11").click(function(){
+    $("audio#music11")[0].play();
+    $("#play11").toggle();
+    $("#pause11").toggle();
+    $("#st11").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause11").click(function(){
+    $("audio#music11")[0].pause();
+    $("#play11").toggle();
+    $("#pause11").toggle();
+    $("#st11").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play12").click(function(){
+    $("audio#music12")[0].play();
+    $("#play12").toggle();
+    $("#pause12").toggle();
+    $("#st12").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause12").click(function(){
+    $("audio#music12")[0].pause();
+    $("#play12").toggle();
+    $("#pause12").toggle();
+    $("#st12").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play13").click(function(){
+    $("audio#music13")[0].play();
+    $("#play13").toggle();
+    $("#pause13").toggle();
+    $("#st13").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause13").click(function(){
+    $("audio#music13")[0].pause();
+    $("#play13").toggle();
+    $("#pause13").toggle();
+    $("#st13").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play14").click(function(){
+    $("audio#music14")[0].play();
+    $("#play14").toggle();
+    $("#pause14").toggle();
+    $("#st14").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause14").click(function(){
+    $("audio#music14")[0].pause();
+    $("#play14").toggle();
+    $("#pause14").toggle();
+    $("#st14").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play15").click(function(){
+    $("audio#music15")[0].play();
+    $("#play15").toggle();
+    $("#pause15").toggle();
+    $("#st15").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause15").click(function(){
+    $("audio#music15")[0].pause();
+    $("#play15").toggle();
+    $("#pause15").toggle();
+    $("#st15").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play16").click(function(){
+    $("audio#music16")[0].play();
+    $("#play16").toggle();
+    $("#pause16").toggle();
+    $("#st16").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause16").click(function(){
+    $("audio#music16")[0].pause();
+    $("#play16").toggle();
+    $("#pause16").toggle();
+    $("#st16").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play17").click(function(){
+    $("audio#music17")[0].play();
+    $("#play17").toggle();
+    $("#pause17").toggle();
+    $("#st17").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause17").click(function(){
+    $("audio#music17")[0].pause();
+    $("#play17").toggle();
+    $("#pause17").toggle();
+    $("#st17").css({
+      "visibility":"hidden",
+    });
+
+  });
+
+  $("#play18").click(function(){
+    $("audio#music18")[0].play();
+    $("#play18").toggle();
+    $("#pause18").toggle();
+    $("#st18").css({
+      "visibility":"visible",
+    });
+  });
+
+  $("#pause18").click(function(){
+    $("audio#music18")[0].pause();
+    $("#play18").toggle();
+    $("#pause18").toggle();
+    $("#st18").css({
+      "visibility":"hidden",
+    });
+
+  });
 
   var audioPlayers = document.getElementsByClassName("audioPlayer");
   var volume = 50;
@@ -98,7 +415,6 @@ $("#volumeSlider").val(volume);
 
 
 
-  $(".hours").draggable();
 
 });
 
