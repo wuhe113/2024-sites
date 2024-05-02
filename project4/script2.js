d03a4353fac614f0c0afe7ea83b805b5d88c04e3
Helen Wu <wuz420@newschool.edu>
2024-05-02 03:13:57 -0400

    Added sun & bg set css

--- project4/script2.js
+++ project4/script2.js
@@ -1,3 +1,33 @@
+const colors = [
+    "rgb(221, 167, 74)",
+    "rgb(45, 154, 253)",
+    "rgb(255, 127, 95)",
+    "rgb(255, 201, 210)",
+    "rgb(89, 149, 194)",
+    "rgb(0, 69, 255)",
+    "rgb(0, 211, 255)",
+    "rgb(0, 111, 212)",
+    "rgb(0, 48, 116)",
+    "rgb(0, 8, 40)",
+    "rgb(255, 61, 40)",
+];
+
+let index = 0;
+
+function changeBackgroundColor() {
+
+    const background = document.getElementById('set1');
+
+    console.log(background);
+
+    background.style.transition = "background 2s ease";
+    background.style.background = colors[index];
+    index = (index + 1) % colors.length;
+}
+
+setInterval(changeBackgroundColor, 5000);
+
+
 $(document).ready(function(){
 
     $.getJSON("data.json", function(data){
@@ -19,5 +49,6 @@ $(document).ready(function(){
         }
         $(".content").html(newhtml);
     });
+
 
 });
