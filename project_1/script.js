a24d394f34ae535a5ef1874e8e9bccada4fba3d3
Helen Wu <wuz420@newschool.edu>
2024-09-08 22:19:51 -0400

    Added more contents*letters click/show js

--- project_1/script.js
+++ project_1/script.js
@@ -16,9 +16,12 @@ function addElement(){
 
     newDiv.classList.add("letter");
 
-    let newContent = document.createTextNode(getRandomLetter());
+    let letterSpan = document.createElement("span");
+        let newContent = document.createTextNode(getRandomLetter());
 
-    newDiv.appendChild(newContent);
+        letterSpan.appendChild(newContent);
+        newDiv.appendChild(letterSpan);
+
 
     // newDiv.style.transform = `translate(${translate}px, ${translate}px)`;
 
@@ -28,18 +31,42 @@ function addElement(){
 
     for (let i = 0; i < 20; i++) {
 
-        let starGird = document.getElementById("starBorder");
-        let starDiv = document.createElement("div");
+        let starGird1 = document.getElementById("starBorder1");
+        let starGird2 = document.getElementById("starBorder2");
+        let starDiv1 = document.createElement("div");
+        let starDiv2 = document.createElement("div");
 
-        starDiv.classList.add("star");
+        starDiv1.classList.add("star");
+        starDiv2.classList.add("star");
 
-        let starContent = document.createTextNode("★");
+        let starContent1 = document.createTextNode("★");
+        let starContent2 = document.createTextNode("★");
 
-        starDiv.appendChild(starContent);
+        starDiv1.appendChild(starContent1);
+        starDiv2.appendChild(starContent2);
 
-        starGird.appendChild(starDiv);
+        starGird1.appendChild(starDiv1);
+        starGird2.appendChild(starDiv2);
 
     }
+
+    // for (let i = 0; i < 2; i++) {
+
+    //     let arrowDiv = document.getElementById("arrow");
+    //     let aDiv = document.createElement("div");
+    
+    //     aDiv.classList.add("arrowLi");
+    
+    //     let arrowContent = document.createTextNode("⬸");
+
+    //         aDiv.appendChild(arrowContent);
+    
+    
+    
+    //     arrowDiv.appendChild(aDiv);
+    //     }
+
+
 
 }
 
@@ -54,6 +81,179 @@ document.addEventListener('mousemove', function(event) {
     }
 });
 
+$(document).ready(function () {
+    addElement();
+
+    $("#grid").on('click', 'span:contains("H")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("H")').css({
+            'visibility':'visible'
+        });
+    });
+
+    $("#grid").on('click', 'span:contains("E")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("e")').css({
+            'visibility':'visible'
+        });
+    });
+
+    $("#grid").on('click', 'span:contains("L")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("l")').css({
+            'visibility':'visible'
+        });
+    });
+
+    $("#grid").on('click', 'span:contains("N")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("n")').css({
+            'visibility':'visible'
+        });
+    });
+
+    $("#grid").on('click', 'span:contains("W")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("W")').css({
+            'visibility':'visible'
+        });
+    });
+
+
+    $("#grid").on('click', 'span:contains("U")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("u")').css({
+            'visibility':'visible'
+        });
+    });
+
+
+    $("#grid").on('click', 'span:contains("I")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("i")').css({
+            'visibility':'visible'
+        });
+    });
+
+    $("#grid").on('click', 'span:contains("O")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("o")').css({
+            'visibility':'visible'
+        });
+    });
+
+    $("#grid").on('click', 'span:contains("R")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("r")').css({
+            'visibility':'visible'
+        });
+    });
+
+    $("#grid").on('click', 'span:contains("C")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("c")').css({
+            'visibility':'visible'
+        });
+    });
+
+    $("#grid").on('click', 'span:contains("D")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("d")').css({
+            'visibility':'visible'
+        });
+    });
+
+    $("#grid").on('click', 'span:contains("T")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("t")').css({
+            'visibility':'visible'
+        });
+    });
+
+    $("#grid").on('click', 'span:contains("A")', function () {
+        $(this).css({
+            'border': '1px solid black',
+            'border-radius': '50%',
+            'padding': '5px'
+        });
+        $('strong:contains("a")').css({
+            'visibility':'visible'
+        });
+    });
+
+    $('#d2').mousedown(function(){
+        $(this).css({
+            'bottom': '20px',
+            'transform': 'skew(0deg, 10deg)'
+        });
+
+        $('#doorknob').css({
+            'bottom': '70px'
+
+        });
+    });
+
+    $('#d2').mouseleave(function(){
+        $(this).css({
+            'bottom': '40px',
+            'transform': 'skew(0deg, -10deg)'
+        });
+
+        $('#doorknob').css({
+            'bottom': '100px'
+
+        });
+    });
+
+});
 
 
 
