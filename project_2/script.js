268eeb703935cb8c4b974cc74e67d095e14ee182
Helen Wu <wuz420@newschool.edu>
2024-10-05 22:18:21 -0400

    Keep adding text

--- project_2/script.js
+++ project_2/script.js
@@ -7,8 +7,13 @@
 let startButton = document.getElementById("button2");
 let hidden = document.getElementById("cover");
 
-startButton.onclick = function(e){
-
+startButton.onclick = function(e) {
     hidden.style.visibility = 'hidden';
 
+    showDiv();
+};
+
+function showDiv() {
+    let div = document.getElementById("content");
+    div.classList.add("visible");
 }
