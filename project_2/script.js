c40d48d8961d4a2e8973185e6b5cd1c956119d89
Helen Wu <wuz420@newschool.edu>
2024-10-06 00:13:33 -0400

    Added scroll js

--- project_2/script.js
+++ project_2/script.js
@@ -8,7 +8,8 @@ let startButton = document.getElementById("button2");
 let hidden = document.getElementById("cover");
 
 startButton.onclick = function(e) {
-    hidden.style.visibility = 'hidden';
+    hidden.style.visibility = "hidden";
+    document.body.style.overflow = "scroll";
 
     showDiv();
 };
@@ -16,4 +17,23 @@ startButton.onclick = function(e) {
 function showDiv() {
     let div = document.getElementById("content");
     div.classList.add("visible");
-}
+};
+
+
+let timesHeader = document.getElementById("headLine");
+
+
+window.onscroll = function(e){
+    if (window.scrollY > 0) {
+        timesHeader.style.top = "-5%";
+    } else {
+        timesHeader.style.top = "0";
+    }
+};
+
+// timesHeader.scroll({
+//     top: 100,
+//     left: 100,
+//     behavior: "smooth",
+//   });
+  
