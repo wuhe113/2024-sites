1178e70a6edf6a857636ce3cabb5d97cbf5e4317
Helen Wu <wuz420@newschool.edu>
2024-10-24 19:57:02 -0400

    Updated

--- project_3/script.js
+++ project_3/script.js
@@ -20,15 +20,34 @@ let name1 = document.getElementById('artist1');
 let mouseLine1 = document.getElementById('line1');
 let mouseLine2 = document.getElementById('line2');
 
+
 const imgArray = ["assets/cr1.jpg", "assets/cr2.jpg", "assets/cr3.jpg"]; 
-// const imgArray2 = ["assets/ma1.jpg", "assets/ma2.jpg", "assets/ma3.jpg"]; 
+const imgArray2 = ["assets/ma1.jpg", "assets/ma2.jpg", "assets/ma3.jpg"]; 
+const imgArray3 = ["assets/e1.jpg", "assets/e2.jpg", "assets/e3.jpg"]; 
+const imgArray4 = ["assets/mf1.jpg", "assets/mf2.jpg", "assets/mf3.jpg"]; 
+
+
+const allImgArrays = [imgArray, imgArray2, imgArray3, imgArray4];
+
+let currentArrayIndex = 0;
 
 function changeBackgroundImg(){
-    let num = Math.floor(Math.random() * (imgArray.length));
-    // let num2 = Math.floor(Math.random() * (imgArray2.length));
-    document.body.style.backgroundImage = `url('${imgArray[num]}')`; 
-    // document.body.style.backgroundImage = `url('${imgArray2[num2]}')`; 
+    let currentArray = allImgArrays[currentArrayIndex];
+    let num = Math.floor(Math.random() * currentArray.length);
+    document.body.style.backgroundImage = `url('${currentArray[num]}')`; 
 }
+
+// function changeBackgroundImg2(){
+//     let num2 = Math.floor(Math.random() * (imgArray2.length));
+//     document.body.style.backgroundImage = `url('${imgArray2[num2]}')`; 
+// }
+
+function switchImageArray() {
+    currentArrayIndex = (currentArrayIndex + 1) % allImgArrays.length;
+    changeBackgroundImg();
+}
+
+
 
 function triggerGlitch() {
     glitchOverlay.classList.add('glitch-active');
@@ -58,6 +77,8 @@ function triggerGlitch() {
 setInterval(triggerGlitch, 5000);
 
 
+
+
 function map(value, low1, high1, low2, high2){
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
@@ -85,17 +106,30 @@ try {
 	const response = await fetch(url, options);
     // const response = await fetch(url2, options2);
 	const result = await response.json();
-
+    
     let followers = document.getElementById('f1');
-    followers.style.width = result.stats.followers * 0.0002 + 'px';
+    followers.style.width = '0px';
+
+    setTimeout(() => { 
+        followers.style.width = result.stats.followers * 0.0002 + 'px'; 
+    }, 2000);
+
+    // let followersRange = map(result.stats.followers, 0, )
 
     let listeners = document.getElementById('f2');
-    listeners.style.width = result.stats.monthlyListeners * 0.00003 + 'px';
+    listeners.style.width = '0px';
+
+    setTimeout(() => { 
+        listeners.style.width = result.stats.monthlyListeners * 0.00003 + 'px';
+    }, 2000);
 
     let ranks = document.getElementById('f3');
+    ranks.style.height= '0px';
 
     let ranksRange = map(result.stats.worldRank, 0, 100, 900, 600);
-    ranks.style.height = ranksRange + 'px';
+    setTimeout(() => { 
+        ranks.style.height = ranksRange + 'px';    
+    }, 10);
 
     // let followers = document.getElementById('f1');
     // followers.style.width = result.data.edge_followed_by.count * 0.0002 + 'px';
@@ -110,29 +144,45 @@ try {
     let ld = document.getElementById('listenersData');
     let rd = document.getElementById('ranksData');
 
-    followers.onmouseenter = function(e){
-        fd.innerHTML = result.stats.followers + " Followers" ;
-    }
+    // followers.onmouseenter = function(e){
+    //     fd.innerHTML = result.stats.followers + " Followers" ;
+    //     followers.style.height = "5.5vw";
+    // }
 
-    followers.onmouseleave = function(e){
-        fd.innerHTML = "";
-    }
+    // followers.onmouseleave = function(e){
+    //     fd.innerHTML = "";
+    //     followers.style.height = "3.5vw";
+    // }
+
+    // followers.onmouseenter = function(e){
+    //     fd.innerHTML = result.data.edge_followed_by.count + " Followers" ;
+    // }
+
+    // followers.onmouseleave = function(e){
+    //     fd.innerHTML = "";
+    // }
+
     
-    listeners.onmouseenter = function(e){
-        ld.innerHTML = result.stats.monthlyListeners + " Monthly Listeners" ;
-    }
+    
+    // listeners.onmouseenter = function(e){
+    //     ld.innerHTML = result.stats.monthlyListeners + " Monthly Listeners" ;
+    //     listeners.style.height = "5.5vw";
+    // }
 
-    listeners.onmouseleave = function(e){
-        ld.innerHTML = "";
-    }
+    // listeners.onmouseleave = function(e){
+    //     ld.innerHTML = "";
+    //     listeners.style.height = "3.5vw";
+    // }
 
-    ranks.onmouseenter = function(e){
-        rd.innerHTML = result.stats.worldRank + " World Rank" ;
-    }
+    // ranks.onmouseenter = function(e){
+    //     rd.innerHTML = result.stats.worldRank + " World Rank" ;
+    //     ranks.style.width = "5.8vw";
+    // }
 
-    ranks.onmouseleave = function(e){
-        rd.innerHTML = "";
-    }
+    // ranks.onmouseleave = function(e){
+    //     rd.innerHTML = "";
+    //     ranks.style.width = "3.8vw"
+    // }
     
 
     
@@ -152,6 +202,110 @@ try {
 }
 
 getData();
+
+const widths = ['600px', '300px', '1200px', '200px', '500px'];
+const widths2 = ['600px', '600px', '600px', '300px', '500px'];
+const height = ['700px', '700px', '700px', '700px', '500px'];
+
+const skew = ['-10deg', '-10deg', '-10deg', '-20deg'];
+
+const left = ['18%', '23%', '23%', '18%'];
+const right = ['12%', '23%', '23%', '12%'];
+
+
+const names = ['First Text', 'MADELINE ARGY', 'EMMA CHAMBERLAIN', 'MUSTAFA'];
+
+// const rotate = ['-70deg', '-80deg', '-80deg', '-80deg'];
+
+
+let currentIndex = 0; 
+
+let textDisplay = document.getElementById('celebrities');
+
+let letterF = document.getElementById('F');
+
+let THE = document.getElementById('logo1');
+let ACE = document.getElementById('logo2');
+
+let nextButton = document.getElementById('next');
+
+nextButton.onclick = function(e){
+
+    currentIndex = (currentIndex + 1) % widths.length;
+
+    // textDisplay.classList.add('slide-out');
+
+
+    textDisplay.innerHTML = names[currentIndex];
+    // textDisplay.style.transform = `rotate(${rotate[currentIndex]})`;
+        
+    //     textDisplay.classList.remove('slide-out');
+    // textDisplay.style.animation = 'none';
+    // void textDisplay.offsetHeight;
+    // textDisplay.style.animation = '';
+    // textDisplay.classList.add('slide-in'); 
+
+    letterF.style.transform = `skew(${skew[currentIndex]})`;
+    letterF1.style.width = widths[currentIndex];
+    letterF2.style.width = widths2[currentIndex];
+    letterF3.style.height = height[currentIndex];
+
+    THE.style.right = right[currentIndex];
+    ACE.style.left = left[currentIndex];
+
+
+
+    
+
+//     const url = 'https://spotify-scraper.p.rapidapi.com/v1/artist/overview?artistId=7GlBOeep6PqTfFi59PTUUN';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': '03118cb151msha37940bec131f3ep1379f0jsn9c1e5387bce7',
+// 		'x-rapidapi-host': 'spotify-scraper.p.rapidapi.com'
+// 	}
+// };
+
+// getData(url, options);
+
+switchImageArray();
+
+};
+
+window.onload = function() {
+    setTimeout(() => { changeBackgroundImg();    }, 5000);
+};
+
+let issue = document.getElementById('issue');
+
+
+let offsetX, offsetY, activeElement;
+
+
+function makeElementDraggable(element) {
+    element.onmousedown = function(e) {
+        activeElement = element;
+
+        offsetX = e.clientX - activeElement.offsetLeft;
+        offsetY = e.clientY - activeElement.offsetTop;
+
+        document.onmousemove = dragElement;
+        document.onmouseup = stopDragElement;
+    };
+}
+
+function dragElement(e) {
+    activeElement.style.left = (e.clientX - offsetX) + 'px';
+    activeElement.style.top = (e.clientY - offsetY) + 'px';
+}
+
+function stopDragElement() {
+    document.onmousemove = null;
+    document.onmouseup = null;
+}
+
+makeElementDraggable(textDisplay);
+makeElementDraggable(issue);
 
 
 
@@ -182,3 +336,11 @@ getData();
 // }
 
 // getData();
+
+
+// $("document").ready(function(){
+
+//     $("#celebrities").draggable();
+
+      
+//     });
