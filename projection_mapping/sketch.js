ab7f7a0901e272d06a3e2f392c2f5de54d115d3d
Helen Wu <wuz420@newschool.edu>
2024-11-17 22:52:45 -0500

    Updated

--- projection_mapping/sketch.js
+++ projection_mapping/sketch.js
@@ -3,6 +3,7 @@ let change1 = document.getElementById("c1");
 let change2 = document.getElementById("s1")
 let change3 = document.getElementById("c2")
 let change4 = document.getElementById("r1")
+let change5 = document.getElementById("s2")
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
@@ -27,16 +28,19 @@ function draw(){
 
     change4.style.width = vol*4000 +'vw';
 
-    // let numLines = int(vol * vol * 500);
-    // for (let i = 0; i < numLines; i++) {
-    //     let x1 = random(width);
-    //     let y1 = random(height);
-    //     let x2 = random(width);
-    //     let y2 = random(height);
-    //     stroke(255);
-    //     strokeWeight(2);
-    //     line(x1, y1, x2, y2);
-    // }
+    change5.style.width = vol*800 +'vw';
+    change5.style.height = vol*300 +'vw';
+
+    let numLines = int(vol * vol * 500);
+    for (let i = 0; i < numLines; i++) {
+        let x1 = random(width);
+        let y1 = random(height);
+        let x2 = random(width);
+        let y2 = random(height);
+        stroke(255);
+        strokeWeight(2);
+        line(x1, y1, x2, y2);
+    }
 
 
 
