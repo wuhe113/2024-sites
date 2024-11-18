0c946972c6add730177e3a4ac075b5b3e21c7009
Helen Wu <wuz420@newschool.edu>
2024-11-17 22:45:14 -0500

    Sound test

---
+++ projection_mapping/sketch.js
@@ -0,0 +1,43 @@
+var mic;
+let change1 = document.getElementById("c1");
+let change2 = document.getElementById("s1")
+let change3 = document.getElementById("c2")
+let change4 = document.getElementById("r1")
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    mic = new p5.AudioIn();
+    mic.start();
+}
+
+function draw(){
+    background(0);
+
+    var vol = mic.getLevel();
+
+    change1.style.width = (vol*200) + 10 +'vw';
+    change1.style.height = (vol*200) + 10 +'vw';
+
+    change2.style.width = (vol*200) + 10 +'vw';
+    change2.style.height = (vol*200) + 10 +'vw';
+
+    change3.style.width = vol*1000 +'vw';
+    change3.style.height = vol*1000 +'vw';
+
+
+    change4.style.width = vol*4000 +'vw';
+
+    // let numLines = int(vol * vol * 500);
+    // for (let i = 0; i < numLines; i++) {
+    //     let x1 = random(width);
+    //     let y1 = random(height);
+    //     let x2 = random(width);
+    //     let y2 = random(height);
+    //     stroke(255);
+    //     strokeWeight(2);
+    //     line(x1, y1, x2, y2);
+    // }
+
+
+
+}
