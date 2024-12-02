30e6d6bed877c9d26eab1f37dc8873e4fa742d6f
Helen Wu <wuz420@newschool.edu>
2024-12-02 01:53:48 -0500

    Projection mapping final project ascii art

---
+++ projection_final/sketch.js
@@ -0,0 +1,58 @@
+const density = '            Ñ@#W$9876543210?!abc;:+=-,._';
+
+// let puppy;
+
+let video;
+
+function preload(){
+    puppy = loadImage("assets/puppy.png");
+}
+
+function setup(){
+    noCanvas();
+
+    video = createCapture(VIDEO);
+    video.size(134, 48);
+
+    asciiDiv = createDiv();
+}
+
+    // image(puppy, 0, 0, width, height);
+function draw(){
+    // let w = width / puppy.width;
+    // let h = height / puppy.height;
+
+    // puppy.loadPixels();
+
+        video.loadPixels();
+
+        let asciiImage = '';
+
+    for(let j = 0; j < video.height; j++){
+        for (let i = 0; i < video.width; i++){
+            const pixelIndex = (i + j * video.width)*4;
+            const r = video.pixels[pixelIndex + 0];
+            const g = video.pixels[pixelIndex + 1];
+            const b = video.pixels[pixelIndex + 2];
+            const avg = (r + g + b) / 3;
+
+
+            const len = density.length;
+            const charIndex = floor(map(avg, 0, 255, 0, len));
+
+
+            // textSize(w);
+            // textAlign(CENTER, CENTER);
+           const c = density.charAt(charIndex);
+
+           if(c == ' ') asciiImage += '&nbsp;'
+           else asciiImage += c;
+        }
+
+        asciiImage += '<br/>'
+        // createDiv(row);
+        // console.log(row);
+    }
+
+    asciiDiv.html(asciiImage);
+}
