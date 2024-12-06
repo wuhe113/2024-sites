a646888d08b956573231cb4afba351bcee277b9e
Helen Wu <wuz420@newschool.edu>
2024-12-06 00:03:37 -0500

    Revised value

--- projection_final/sketch.js
+++ projection_final/sketch.js
@@ -99,14 +99,17 @@ function draw() {
 
   for (let j = 0; j < video.height; j++) {
     for (let i = 0; i < video.width; i++) {
-      const pixelIndex = (i + j * video.width) * 4;
+      // Flip the video horizontally
+      const flippedX = video.width - i - 1; // Reverse the x-coordinate
+
+      const pixelIndex = (flippedX + j * video.width) * 4;
       const r = video.pixels[pixelIndex + 0];
       const g = video.pixels[pixelIndex + 1];
       const b = video.pixels[pixelIndex + 2];
       const avg = (r + g + b) / 3;
 
       const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, len, 0));
+      const charIndex = floor(map(avg, 0, 220, len, 0));
 
       const c = density.charAt(charIndex);
       asciiImage += c === ' ' ? '&nbsp;' : c;
@@ -116,3 +119,4 @@ function draw() {
 
   asciiDiv.html(asciiImage);
 }
+
