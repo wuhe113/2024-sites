6f52b6dc71ab8ff68e6af61b3f48e8b1d2e38757
Helen Wu <wuz420@newschool.edu>
2025-09-19 17:20:32 -0400

    Revised*i hope this time it works properly

--- projection_mapping/sketch.js
+++ projection_mapping/sketch.js
@@ -50,57 +50,69 @@
 // }
 
 
+let mic;
 let micStarted = false;
+let change1, change2, change3, change4, change5;
 
 function setup(){
-    createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+  mic = new p5.AudioIn();
 
-    change1 = document.getElementById("c1");
-    change2 = document.getElementById("s1")
-    change3 = document.getElementById("c2")
-    change4 = document.getElementById("r1")
-    change5 = document.getElementById("s2")
-
-    mic = new p5.AudioIn();
+  change1 = document.getElementById("c1");
+  change2 = document.getElementById("s1");
+  change3 = document.getElementById("c2");
+  change4 = document.getElementById("r1");
+  change5 = document.getElementById("s2");
 }
 
 function draw(){
-    background(0);
+  background(0);
 
-    if (micStarted) {
-        let vol = mic.getLevel();
+  if (!micStarted) {
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text("Click to start audio", width/2, height/2);
+    return;
+  }
 
-        change1.style.width = (vol*200) + 10 +'vw';
-        change1.style.height = (vol*200) + 10 +'vw';
+  let vol = mic.getLevel();
 
-        change2.style.width = (vol*200) + 10 +'vw';
-        change2.style.height = (vol*200) + 10 +'vw';
+  if (change1 && change2 && change3 && change4 && change5) {
+    change1.style.width = (vol*200) + 10 +'vw';
+    change1.style.height = (vol*200) + 10 +'vw';
 
-        change3.style.width = vol*1000 +'vw';
-        change3.style.height = vol*1000 +'vw';
+    change2.style.width = (vol*200) + 10 +'vw';
+    change2.style.height = (vol*200) + 10 +'vw';
 
-        change4.style.width = vol*4000 +'vw';
+    change3.style.width = vol*1000 +'vw';
+    change3.style.height = vol*1000 +'vw';
 
-        change5.style.width = vol*800 +'vw';
-        change5.style.height = vol*300 +'vw';
+    change4.style.width = vol*4000 +'vw';
 
-        let numLines = int(vol * vol * 500);
-        for (let i = 0; i < numLines; i++) {
-            let x1 = random(width);
-            let y1 = random(height);
-            let x2 = random(width);
-            let y2 = random(height);
-            stroke(255);
-            strokeWeight(2);
-            line(x1, y1, x2, y2);
-        }
-    }
+    change5.style.width = vol*800 +'vw';
+    change5.style.height = vol*300 +'vw';
+  }
+
+  let numLines = int(vol * vol * 500);
+  for (let i = 0; i < numLines; i++) {
+    let x1 = random(width);
+    let y1 = random(height);
+    let x2 = random(width);
+    let y2 = random(height);
+    stroke(255);
+    strokeWeight(2);
+    line(x1, y1, x2, y2);
+  }
 }
 
-
 function mousePressed() {
-    if (!micStarted) {
-        mic.start();
-        micStarted = true;
-    }
+  if (!micStarted) {
+    mic.start();
+    micStarted = true;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
