4feaaf94a13f7d2149aaf72220e5fd05a247c861
Helen Wu <wuz420@newschool.edu>
2025-09-19 22:24:59 -0400

    Added getAudioContext().resume()//manually unlocks the browser's web audio system

--- projection_mapping/sketch.js
+++ projection_mapping/sketch.js
@@ -54,44 +54,59 @@ let mic;
 let micStarted = false;
 let change1, change2, change3, change4, change5;
 
-function setup(){
+
+function setup() {
   createCanvas(windowWidth, windowHeight);
-  mic = new p5.AudioIn();
 
   change1 = document.getElementById("c1");
   change2 = document.getElementById("s1");
   change3 = document.getElementById("c2");
   change4 = document.getElementById("r1");
   change5 = document.getElementById("s2");
+
 }
 
-function draw(){
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+
+//   change1 = document.getElementById("c1");
+//   change2 = document.getElementById("s1");
+//   change3 = document.getElementById("c2");
+//   change4 = document.getElementById("r1");
+//   change5 = document.getElementById("s2");
+
+//   mic = new p5.AudioIn();
+// }
+
+function draw() {
   background(0);
 
   if (!micStarted) {
     fill(255);
     textSize(32);
     textAlign(CENTER, CENTER);
-    text("Click to start audio", width/2, height/2);
+    text("Click anywhere to start", width / 2, height / 2);
     return;
   }
 
   let vol = mic.getLevel();
+  console.log("Volume:", vol);
+  
 
   if (change1 && change2 && change3 && change4 && change5) {
-    change1.style.width = (vol*200) + 10 +'vw';
-    change1.style.height = (vol*200) + 10 +'vw';
+    change1.style.width = (vol * 200 + 10) + 'vw';
+    change1.style.height = (vol * 200 + 10) + 'vw';
 
-    change2.style.width = (vol*200) + 10 +'vw';
-    change2.style.height = (vol*200) + 10 +'vw';
+    change2.style.width = (vol * 200 + 10) + 'vw';
+    change2.style.height = (vol * 200 + 10) + 'vw';
 
-    change3.style.width = vol*1000 +'vw';
-    change3.style.height = vol*1000 +'vw';
+    change3.style.width = vol * 1000 + 'vw';
+    change3.style.height = vol * 1000 + 'vw';
 
-    change4.style.width = vol*4000 +'vw';
+    change4.style.width = vol * 4000 + 'vw';
 
-    change5.style.width = vol*800 +'vw';
-    change5.style.height = vol*300 +'vw';
+    change5.style.width = vol * 800 + 'vw';
+    change5.style.height = vol * 300 + 'vw';
   }
 
   let numLines = int(vol * vol * 500);
@@ -106,12 +121,28 @@ function draw(){
   }
 }
 
+// function mousePressed() {
+//   if (!micStarted) {
+//     mic.start();
+//     micStarted = true;
+//   }
+// }
+
 function mousePressed() {
   if (!micStarted) {
-    mic.start();
-    micStarted = true;
+    getAudioContext().resume().then(() => {
+      mic = new p5.AudioIn();
+      mic.start(() => {
+        console.log("Microphone started");
+        micStarted = true;
+      }, () => {
+        console.error("Microphone failed to start");
+      });
+    });
   }
 }
+
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
